5c86e16afd523662f7773cadeef41b10e547f332
Robson Luan <rluan95@hotmail.com>
2022-07-17 23:07:40 -0300

    Atualização2

--- Aula27.js
+++ Aula27.js
@@ -3,6 +3,9 @@ var imgs=[];
 var slider;
 var imgAtual;
 var maxImg;
+var tempoTroca;
+var verTempo;
+var verLoad;
 
 //FUNÇAO PARA DECLARAR QUAL NOME DA IMAGEM E ONDE BUSCAR
 
@@ -23,20 +26,36 @@ function carregarImg(img){
 
 }
 function Inicia(){
+    verLoad=document.getElementById("dvBarra")
     preCarregamento();
-    imgAtual=1;
-    maxImg=imgs.length;
+    imgAtual=0;
+    maxImg=imgs.length-1;
     slider=document.getElementById("dvslider");
     carregarImg(imgAtual);
+    tempoTroca=0;
+    anima();
 }
 
+function anima(){
+    tempoTroca++;
+    if (tempoTroca>=500){
+        tempoTroca=0;
+        troca(1);
+    }
+    verTempo=(tempoTroca/5)
+    verLoad.style.width=verTempo+"%"
+    window.requestAnimationFrame(anima);
+
+}
 
 //função para trocar as imagens
-setInterval(troca,2000)
-function troca(){
-    imgAtual++;
+function troca(dir){
+    tempoTroca=0;
+    imgAtual+=dir;
     if (imgAtual>maxImg){
         imgAtual=0;
+    }else if (imgAtual<0){
+        imgAtual=maxImg
     }
     carregarImg(imgAtual);
 }
